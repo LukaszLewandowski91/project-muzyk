05baedd78b166fcafc6d1d2f714d4e3c5bc0664a
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-03-05 19:03:12 +0100

    add activatePage and initPages methods in app.js

---
+++ src/js/settings.js
@@ -0,0 +1,19 @@
+export const select = {
+  templateOf: {
+    homePage: '#template-home-page',
+  },
+  containerOf: {
+    navs: '.main-nav',
+    pages: '#pages',
+    homePage: '.home-wrapper',
+  },
+  nav: {
+    links: '.main-nav a',
+  },
+};
+
+export const classNames = {
+  navs: {
+    active: 'active',
+  },
+};
