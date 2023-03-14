0856473eafd59e3fb55f0ead006529ec95550013
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-03-14 15:50:50 +0100

    add dist

---
+++ dist/js/settings.js
@@ -0,0 +1,58 @@
+export const select = {
+  templateOf: {
+    homePage: '#template-home-page',
+    searchPage: '#template-search-page',
+    discoverPage: '#template-discover-page',
+  },
+  containerOf: {
+    navs: '.main-nav',
+    pages: '#pages',
+    homePage: '.home-wrapper',
+    music: '.music',
+    musicOnSearch: '.music-search',
+    subscribe: '.container-subscribe',
+    searchPage: '.search-wrapper',
+    containerMusic: '.searching',
+    discoverPage: '.discover-wrapper',
+    musicDiscover: '.music-discover',
+  },
+  nav: {
+    links: '.main-nav a',
+  },
+  song: {
+    categories: '.categories',
+  },
+  button: {
+    search: '.buttonSearch',
+    text: '.fragmentText',
+    valueSong: '.founder-song',
+  },
+};
+
+export const classNames = {
+  navs: {
+    active: 'active',
+  },
+};
+
+export const settings = {
+  db: {
+    url:
+      '//' +
+      window.location.hostname +
+      (window.location.hostname == 'localhost' ? ':3131' : ''),
+    songs: 'songs',
+  },
+};
+
+export const templates = {
+  homePage: Handlebars.compile(
+    document.querySelector(select.templateOf.homePage).innerHTML
+  ),
+  searchPage: Handlebars.compile(
+    document.querySelector(select.templateOf.searchPage).innerHTML
+  ),
+  discoverPage: Handlebars.compile(
+    document.querySelector(select.templateOf.discoverPage).innerHTML
+  ),
+};
